a38b6c149ecb675a122d1031d05cba8151a6b9fc
Ian McCarthy <ian.mccarthy@emory.edu>
2026-03-30 12:41:47 -0400

    Initial project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Physician Variation in Treatment Intensity
+
+Does medical training shape a physician's baseline practice intensity, or is treatment style driven primarily by peers and practice environment? Uses Medicare claims data (NSTEMI/cardiac catheterization) and a physician mover design.
---
+++ code/0-setup.R
@@ -0,0 +1,11 @@
+# Meta --------------------------------------------------------------------
+
+## Author:        Ian McCarthy
+## Date Created:  2026-03-30
+## Description:   Activates renv and loads packages.
+
+# renv activation ---------------------------------------------------------
+source("renv/activate.R")
+
+# Packages ----------------------------------------------------------------
+library(tidyverse)
